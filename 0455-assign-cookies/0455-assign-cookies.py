16e357371e917921bf8bb5f8551971267bd760f6
NitishSaibewar <saibewarnitish@gmail.com>
2026-07-20 10:38:24 +0530

    Time: 33 ms (57.42%), Space: 14 MB (51.05%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -2,8 +2,8 @@ class Solution(object):
     def findContentChildren(self, g, s):
         g.sort()
         s.sort()
-        i = 0  # child pointer
-        j = 0  # cookie pointer
+        i = 0 
+        j = 0  
         while i < len(g) and j < len(s):
             if s[j] >= g[i]:
                 i += 1
